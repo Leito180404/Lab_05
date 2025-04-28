19d4c11f34228f3689f0b8bf59b50f24b918d194
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-04-28 08:32:57 -0500

    Update GestorDeTareas.java

--- Ejercicios/GestorDeTareas.java
+++ Ejercicios/GestorDeTareas.java
@@ -1,5 +1,72 @@
 package Ejercicios;
+import java.util.LinkedList;
+import java.util.List;
 
-public class GestorDeTareas {
-    
+public class GestorDeTareas<T> {
+    private List<T> tareas;
+
+    public GestorDeTareas() {
+        tareas = new LinkedList<>(); 
+    }
+
+    public void agregarTarea(T tarea) {
+        tareas.add(tarea);
+    }
+
+    public boolean eliminarTarea(T tarea) {
+        return tareas.remove(tarea);  
+    }
+
+    public boolean contieneTarea(T tarea) {
+        return tareas.contains(tarea);  
+    }
+
+    public void imprimirTareas() {
+        for (T tarea : tareas) {
+            System.out.println(tarea);
+        }
+    }
+
+    public int contarTareas() {
+        return tareas.size(); 
+    }
+
+
+    public T obtenerTareaMasPrioritaria() {
+        if (tareas.isEmpty() || !(tareas.get(0) instanceof Tarea)) {
+            return null; 
+        }
+
+        Tarea tareaMasPrioritaria = (Tarea) tareas.get(0);
+        for (T tarea : tareas) {
+            if (tarea instanceof Tarea) {
+                Tarea tareaActual = (Tarea) tarea;
+                if (tareaActual.getPrioridad() < tareaMasPrioritaria.getPrioridad()) {
+                    tareaMasPrioritaria = tareaActual;
+                }
+            }
+        }
+        return (T) tareaMasPrioritaria;
+    }
+
+    public void invertirTareas() {
+        List<T> listaInvertida = new LinkedList<>();
+        for (int i = tareas.size() - 1; i >= 0; i--) {
+            listaInvertida.add(tareas.get(i));
+        }
+        tareas = listaInvertida;
+    }
+
+    public List<T> getTareas() {
+        return tareas;
+    }
+    //metodo buscar elemento ejercicio 1
+    public static <T> boolean buscarElemento(List<T> lista, T elemento) {
+        for (T item : lista) {
+            if (item.equals(elemento)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
