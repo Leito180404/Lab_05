74622fb4dd769043290ea946bab7227707d0ec45
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-04-28 08:32:24 -0500

    Update Tarea.java

--- Ejercicios/Tarea.java
+++ Ejercicios/Tarea.java
@@ -1,5 +1,35 @@
 package Ejercicios;
 
 public class Tarea {
-    
+    private String titulo;
+    private int prioridad;
+   
+    public Tarea(String titulo, int prioridad) {
+        this.titulo = titulo;
+        this.prioridad = prioridad;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(int prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarea{" +
+                "titulo='" + titulo + '\'' +
+                ", prioridad=" + prioridad +
+                '}';
+    }
 }
