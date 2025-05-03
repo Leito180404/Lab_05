4c2b6eb3e70709a7a9f4e93c6df9585c0f386bba
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-03 14:58:50 -0500

    Update insertaralfinal.java

--- Ejercicios/insertaralfinal.java
+++ Ejercicios/insertaralfinal.java
@@ -17,7 +17,7 @@ public class insertaralfinal {
             return head;
         } //ejercicio3
 
-        public static <T> int contarNodos(Node<T> head) {
+        public static <T> int contNo(Node<T> head) {
             int contador = 0;
             Node<T> actual = head;
         
@@ -28,5 +28,53 @@ public class insertaralfinal {
         
             return contador;
         }//ejercicio 4
+
+        public static <T> boolean ifsoniguales(Node<T> lista1, Node<T> lista2) {
+            Node<T> actual1 = lista1;
+            Node<T> actual2 = lista2;
+        
+            while (actual1 != null && actual2 != null) {
+                if (!actual1.getDato().equals(actual2.getDato())) {
+                    return false;
+                }
+                actual1 = actual1.getNext();
+                actual2 = actual2.getNext();
+            }
+        
+            return actual1 == null && actual2 == null;  // Ambos deben ser null al final
+        }//ejercicio 5
+        
+        public static <T> Node<T> concatenarListas(Node<T> lista1, Node<T> lista2) {
+            Node<T> nuevaLista = null;
+            Node<T> actual = null;
+        
+            // Agregar los elementos de lista1 a nuevaLista
+            Node<T> temp = lista1;
+            while (temp != null) {
+                if (nuevaLista == null) {
+                    nuevaLista = new Node<>(temp.getDato());
+                    actual = nuevaLista;
+                } else {
+                    actual.setNext(new Node<>(temp.getDato()));
+                    actual = actual.getNext();
+                }
+                temp = temp.getNext();
+            }
+        
+            // Agregar los elementos de lista2 a nuevaLista
+            temp = lista2;
+            while (temp != null) {
+                if (nuevaLista == null) {
+                    nuevaLista = new Node<>(temp.getDato());
+                    actual = nuevaLista;
+                } else {
+                    actual.setNext(new Node<>(temp.getDato()));
+                    actual = actual.getNext();
+                }
+                temp = temp.getNext();
+            }
+        
+            return nuevaLista;
+        }
         
 }
